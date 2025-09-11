b421f220d4b9792ab3e402258b05e84ba9727df9
= <=>
2025-09-11 13:11:28 +0700

    First commit dengan folder project1

---
+++ project1/bin/project1.dart
@@ -0,0 +1,15 @@
+import 'package:project1/project1.dart' as project1;
+const String nama = 'Dini';
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project1.calculate()}!');
+  // satu line comment
+  /*
+  multi line comment
+  banyak line comment
+  */
+  // constanta dan final
+  final String nama2 = 'Elminigtyas';
+  print("Nama saya $nama $nama2");
+  
+}
