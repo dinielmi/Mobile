341300dbeec7a0dc7478ab3886db62287ccac653
= <=>
2025-09-11 13:23:43 +0700

    variable

--- project1/bin/project1.dart
+++ project1/bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 const String nama = 'Dini';
 
@@ -11,5 +13,18 @@ void main(List<String> arguments) {
   // constanta dan final
   final String nama2 = 'Elminigtyas';
   print("Nama saya $nama $nama2");
-  
+  //  variabel
+  int umur = 20;
+  double tinggi = 151.5;
+  bool jenis_kelamin = false; //true = perempuan, false = laki-laki
+  String alamat = 'Jl. Melati No. 20';
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi cm");
+  // stdout.writeln("Saya ${jenis_kelamin ? 'Perempuan' : 'Laki-laki'}");
+  if (jenis_kelamin==true) {
+    stdout.writeln("Saya Perempuan");
+  } else {
+    stdout.writeln("Saya Laki-laki"); 
+  }
+  stdout.writeln("Alamat saya $alamat");
 }
