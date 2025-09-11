7bcb759161e928a9441e780ded1693a215e8a023
= <=>
2025-09-11 13:52:43 +0700

    variable

--- project1/bin/project1.dart
+++ project1/bin/project1.dart
@@ -27,4 +27,49 @@ void main(List<String> arguments) {
     stdout.writeln("Saya Laki-laki"); 
   }
   stdout.writeln("Alamat saya $alamat");
+  num nilai = 90.5;
+  stdout.writeln("Nilai saya $nilai");
+  dynamic bebas = 16;
+  stdout.writeln("Nilai dari dynamic adalah $bebas");
+
+  // String angka1 = "15";
+  // int angka2 = int.parse(angka1);
+  // int angka3 = bebas;
+
+  // list
+  List <String> hobby = ['Membaca', 'Menulis', 'Menononton'];
+  hobby.add("Main Game");
+  stdout.writeln("Hobby saya: $hobby");
+
+  Map<String, String> data = {
+    'nama': 'Dini Elminigtyas',
+    'alamat': 'Batu',
+    'pekerjaan': 'Mahasiswa'
+  };
+
+  data.addAll({'hobby': 'Membaca'});
+  stdout.writeln("Data saya: $data");
+  stdout.writeln("Nama yang ada di data ${data['nama']}");
+
+try {
+  // Code that might throw an exception
+  int result = 10 ~/ 0; // This will throw an IntegerDivisionByZeroException
+
+  // Handles the specific IntegerDivisionByZeroException
+  print("Cannot divide by zero!");
+} on IntegerDivisionByZeroException {
+  print("Cannot divide by zero!");
+} catch (e) {
+  // Handles any other type of exception and provides the exception object
+  print("An unexpected error occurred: $e");
+} finally {
+  // Code that always executes, regardless of whether an exception occurred
+  print("Execution complete.");
+}
+
+String? kendaraan;
+String kendaraan1 = "bajai";
+stdout.writeln("kendaraan $kendaraan");
+
+
 }
