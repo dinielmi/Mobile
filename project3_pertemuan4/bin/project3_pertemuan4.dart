23c0d5c6265a82476f7884f54d58ccd616c148ff
= <=>
2025-09-19 11:04:20 +0700

    first commit project3

---
+++ project3_pertemuan4/bin/project3_pertemuan4.dart
@@ -0,0 +1,5 @@
+import 'package:project3_pertemuan4/project3_pertemuan4.dart' as project3_pertemuan4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project3_pertemuan4.calculate()}!');
+}
---
+++ project3_pertemuan4/test/project3_pertemuan4_test.dart
@@ -0,0 +1,8 @@
+import 'package:project3_pertemuan4/project3_pertemuan4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
