e4f6e66109a1c82030d08122cf801b874abf8f06
= <=>
2025-09-19 11:21:52 +0700

    fixed list

--- project3_pertemuan4/bin/project3_pertemuan4.dart
+++ project3_pertemuan4/bin/project3_pertemuan4.dart
@@ -1,5 +1,20 @@
+import 'dart:io';
+
 import 'package:project3_pertemuan4/project3_pertemuan4.dart' as project3_pertemuan4;
 
+
 void main(List<String> arguments) {
-  print('Hello world: ${project3_pertemuan4.calculate()}!');
+
+  // fixed List
+  // print('Hello world: ${project3_pertemuan4.calculate()}!');
+var fl = List<int>.filled(4,1);    
+fl[0] = 1;
+fl[1] = 2;
+fl[2] = 3;
+fl[3] = 4;
+// fl[4] = 5;
+
+//cetak
+stdout.writeln(fl);
+
 }
