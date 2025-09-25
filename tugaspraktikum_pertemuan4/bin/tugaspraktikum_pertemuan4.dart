617c3c43fc26b373b5c93ba23ad1fae1c5f69221
= <=>
2025-09-25 18:38:02 +0700

    first commit

---
+++ tugaspraktikum_pertemuan4/bin/tugaspraktikum_pertemuan4.dart
@@ -0,0 +1,5 @@
+import 'package:tugaspraktikum_pertemuan4/tugaspraktikum_pertemuan4.dart' as tugaspraktikum_pertemuan4;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${tugaspraktikum_pertemuan4.calculate()}!');
+}
