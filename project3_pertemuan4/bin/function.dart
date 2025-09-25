07ad8f716f03f02c58471e026dfbab2f9364c69f
= <=>
2025-09-25 18:10:24 +0700

    function

---
+++ project3_pertemuan4/bin/function.dart
@@ -0,0 +1,9 @@
+import 'dart:io'; 
+
+void sapa(String nama, int umur){
+  print("Halo namaku $nama, umurku $umur tahun");
+}
+
+void main() {
+  sapa("Diniel", 20); //parameternya nama -> "Diniel", umur -> 20
+}
