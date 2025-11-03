671daf1e2887d2b1534bc3aaa17c713bae46d420
= <=>
2025-11-03 13:08:01 +0700

    first commit

--- project7_kamerasib/test/widget_test.dart
+++ project7_kamerasib/test/widget_test.dart
@@ -12,8 +12,7 @@ import 'package:project7_kamerasib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // Build our app and trigger a frame
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
